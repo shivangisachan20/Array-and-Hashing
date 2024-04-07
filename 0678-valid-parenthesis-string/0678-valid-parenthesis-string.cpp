5e5746c2da4c2e5e3d34c6fb12b4a6833f07327c
Shivangi Sachan <shivangisachan900@gmail.com>
2024-04-07 16:22:23 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (27.25%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool checkValidString(string s)
+    {
+       int n=s.size();
+        stack<int>open,star;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(') open.push(i);
+            else if(s[i]=='*') star.push(i);
+               else
+               {
+                   if(!open.empty()) open.pop();
+                   else
+                   {
+                       if(!star.empty())
+                           star.pop();
+                       else
+                           return false;
+                       
+                   }
+               }
+            }
+        while(!open.empty()){
+            
+            if(star.empty()) return false;
+            
+            else{
+                
+                if(open.top() < star.top()) // checking for index of open and star
+                {
+                    open.pop();
+                    star.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
