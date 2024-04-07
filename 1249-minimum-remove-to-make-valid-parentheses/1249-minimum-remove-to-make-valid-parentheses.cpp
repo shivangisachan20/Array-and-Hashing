366253f96c206c31ac05938a138f1fb02863e88c
Shivangi Sachan <shivangisachan900@gmail.com>
2024-04-07 15:33:59 +0530

    Time: 15 ms (92.60%), Space: 11.7 MB (86.47%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -9,10 +9,15 @@ public:
       else s[i] = '*';
     }
   }
-  while (!st.empty()) {
+  while (!st.empty()) 
+  {
+// st.top() retrieves the element on the top of the stack st. This element represents the index of an opening parenthesis in the string s
+      
     s[st.top()] = '*';
-    st.pop();
+  st.pop();
   }
+        
+// The combination of std::remove and std::erase allows you to remove matching elements from the container so that container would actually get truncated if elements were removed.
   s.erase(remove(s.begin(), s.end(), '*'), s.end());
   return s;
 }
