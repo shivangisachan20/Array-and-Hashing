3e48e46db7a5e443d2341820549e0d994e9977a1
Shivangi Sachan <shivangisachan900@gmail.com>
2024-04-30 23:25:02 +0530

    Time: 16 ms (60.51%), Space: 8.5 MB (96.18%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,29 +1,35 @@
-class Solution 
-{
+class Solution {
+    // Memoization
 public:
-    int method(int i1,int i2, string ring, string key,int m,int n, vector<vector<int>>&dp)
+    int t[101][101];
+    int countsteps(int ringIndex,int i, int n)
     {
-        if(i2==n)
-          return 0;
-        if(dp[i1][i2]!=-1)
-          return dp[i1][i2];
-        int ans = INT_MAX;
-        for(int i=0;i<m;i++)
-        {
-            if(ring[i]==key[i2])
-            {
-                int count = min(abs(i1-i),m-abs(i1-i));
-                ans = min(ans, count + method(i, i2+1, ring, key, m, n,dp));
-            }
-        }
-        return dp[i1][i2]=ans;
+        int dist=abs(i-ringIndex);
+        int wraparound=(n-dist);
+        return min(dist,wraparound);
     }
-    int findRotateSteps(string ring, string key)
+    int solve(int ringIndex,int keyIndex,string& ring, string& key)
     {
-        int i1=0,i2=0;
-        int n = key.size();
-        int m = ring.size();
-        vector<vector<int>>dp(m, vector<int>(n,-1));
-        return method(i1,i2,ring, key,m,n,dp)+n;
+        if(keyIndex==key.length()) return 0;
+        
+        if(t[ringIndex][keyIndex]!=-1)
+            return t[ringIndex][keyIndex];
+        
+        int result=INT_MAX;
+        for(int i=0;i<ring.length();i++)
+        {
+            if(ring[i]==key[keyIndex])
+            {
+                int totalsteps=countsteps(ringIndex,i,ring.length())+1+
+                    solve(i,keyIndex+1,ring,key);
+                result=min(result,totalsteps);
+            }
+            
+        }
+        return t[ringIndex][keyIndex]=result;
+    }
+    int findRotateSteps(string ring, string key) {
+        memset(t,-1,sizeof(t));
+        return solve(0,0,ring,key);
     }
 };
