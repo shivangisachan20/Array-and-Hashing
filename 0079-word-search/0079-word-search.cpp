0bed5482198afc5da5d243becdb698993c9ab417
Shivangi Sachan <shivangisachan900@gmail.com>
2024-05-08 03:32:02 +0530

    Time: 642 ms (50.34%), Space: 10.7 MB (51.73%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,46 @@
-// Backtracking implmentation
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        for(int i = 0; i < board.size(); ++i)
-        {
-            for(int j = 0; j < board[0].size(); ++j)
-            {
-                if(adjacentSearch(board, word, i, j, 0))
-                    return true;
-            }
+  int m, n;
+  vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+  bool find(vector<vector<char>>& board, vector<vector<bool>>& visited, int i, int j, int idx, string& word) {
+    if (idx >= word.length()) {
+      return true; // Word found
+    }
+
+    if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || board[i][j] != word[idx]) {
+      return false; // Invalid cell or visited or mismatch
+    }
+
+    visited[i][j] = true; // Mark cell as visited
+
+    for (auto& dir : directions) {
+      int i_ = i + dir[0];
+      int j_ = j + dir[1];
+
+      if (find(board, visited, i_, j_, idx + 1, word)) {
+        return true; // Found word in a direction
+      }
+    }
+
+    visited[i][j] = false; // Unmark cell after backtracking
+
+    return false; // Word not found starting from current cell
+  }
+
+  bool exist(vector<vector<char>>& board, string word) {
+    m = board.size();
+    n = board[0].size();
+    vector<vector<bool>> visited(m, vector<bool>(n, false)); // Initialize visited set
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (board[i][j] == word[0] && find(board, visited, i, j, 0, word)) {
+          return true; // Word found starting from a cell
         }
-        return false;
-    
+      }
     }
-protected:
-    bool adjacentSearch(vector<vector<char>>& board, const string& word, int i, int j, int index)
-    {
-        if(index == word.size()) return true; // end condition
-        if(i < 0 || j < 0 || i > board.size()-1 || j > board[0].size()-1) return false; // boundary of matrix
-        if(board[i][j] != word[index]) return false; // do not match
-        // match!
-        board[i][j] = '*'; // change the content, to avoid duplicated search
-        bool furtherSearch =  adjacentSearch(board, word, i+1, j, index+1) || // up
-                              adjacentSearch(board, word, i-1, j, index+1) || // down
-                              adjacentSearch(board, word, i, j-1, index+1) || // left
-                              adjacentSearch(board, word, i, j+1, index+1);   // right
-        
-        board[i][j] = word[index]; // modify it back!
-        return furtherSearch;
-    }
+
+    return false; // Word not found in the entire board
+  }
 };
