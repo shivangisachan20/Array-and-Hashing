578cd1c21c5f2aae4bf43d59856ee22fad5c3343
Shivangi Sachan <shivangisachan900@gmail.com>
2024-05-22 23:34:18 +0530

    Time: 165 ms (18.17%), Space: 140 MB (20.71%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -15,26 +15,23 @@ return true;
     
 void backtrack(string &s, int idx, vector<string> curr, vector<vector<string>> &result) 
 {
-
-if(idx == n) 
-{
+    if(idx == n) 
+   {
     result.push_back(curr);
     return;
-}
-  for(int i = idx; i<n; i++) 
-     {
+   }
+for(int i = idx; i<n; i++) 
+{
          if(isPalindrome(s, idx, i)) 
             {
                 curr.push_back(s.substr(idx, i-idx+1));
                 backtrack(s, i+1, curr, result);
                 curr.pop_back();
-                
             }
      }
-        
 }
 vector<vector<string>> partition(string s) 
-    {
+{
         n = s.length();
         vector<vector<string>> result;
         vector<string> curr;
