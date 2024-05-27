adc6575df70faf3040f11bfe9565bf5a4f577cae
Shivangi Sachan <shivangisachan900@gmail.com>
2024-05-27 11:39:42 +0530

    Time: 24 ms (91.53%), Space: 9.7 MB (88.75%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int M = 1e9+7;
-    int t[100001][2][3];
-    int solve(int n,int Absent,int consecutivelate)
-    {
-       if(Absent >= 2 || consecutivelate >= 3) {
-            return 0;
+
+    int checkRecord(int n) {
+        int t[100001][2][3]; 
+
+        //t[i][A][L] = # award possibilities till ith day , having j absence, k consecutive lates
+
+        //Base Case - 0 number of days. So, t[0][A][L], we return 1
+        for (int A = 0; A <= 1; ++A) {
+            for (int L = 0; L <= 2; ++L) {
+                t[0][A][L] = 1;
+            }
         }
-        if(n==0) return 1;
-        
-        if(t[n][Absent][consecutivelate]!=-1)
-            return t[n][Absent][consecutivelate];
-        
-        int A = solve(n-1,Absent+1,0);
-        int L= solve(n-1,Absent,(consecutivelate+1));
-        int P=solve(n-1,Absent,0);
-        return t[n][Absent][consecutivelate]=((A+L)%M+P)%M;
-    }
-    int checkRecord(int n) 
-    {
-      memset(t,-1,sizeof(t));
-      return solve(n,0,0);
-        
-        
+
+        for(int i = 1; i <= n; i++) {
+            for(int A = 0; A <= 1; A++) {
+                for(int L = 0; L <= 2; L++) {
+
+                    long long result = t[i-1][A][0];         // P ---> solve(n-1, absent, 0) % M;
+                    result += (L < 2 ? t[i-1][A][L+1] : 0);  // L ---> solve(n-1, absent, consecutive_late+1) % M;
+                    result += (A == 0 ? t[i-1][A+1][0]: 0);  // A ---> solve(n-1, absent+1, 0) % M;
+                    
+                    t[i][A][L] = result % M;
+                }
+            }
+        }
+
+        return t[n][0][0]; //---> return solve(n, 0, 0);
     }
 };
