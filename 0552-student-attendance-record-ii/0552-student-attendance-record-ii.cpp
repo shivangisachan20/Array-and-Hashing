875adb2a4d5124aa3683e702aac3855774ac149c
Shivangi Sachan <shivangisachan900@gmail.com>
2024-05-27 11:28:26 +0530

    Time: 177 ms (66.25%), Space: 17.5 MB (80.59%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,26 +1,27 @@
-class Solution 
-{
+class Solution {
 public:
-int dp[100005][2][3];
-int mod=1e9+7;
-long long int solve(int n,int a,int l)
-{
-    if(a==2 || l==3)
-        return 0;
-    if(n==0)
-        return 1;
-    if(dp[n][a][l]!=-1)
-        return dp[n][a][l];
-    long long int ans=0;
-    ans=solve(n-1,a,0)%mod;//present
-    ans=(ans+solve(n-1,a+1,0)%mod)%mod;//absent
-    ans=(ans+solve(n-1,a,l+1)%mod)%mod;//late
-    return dp[n][a][l]=ans;
-}
-int checkRecord(int n)
-{
-    memset(dp,-1,sizeof(dp));
-    return solve(n,0,0);
-}
-
+    int M = 1e9+7;
+    int t[100001][2][3];
+    int solve(int n,int Absent,int consecutivelate)
+    {
+       if(Absent >= 2 || consecutivelate >= 3) {
+            return 0;
+        }
+        if(n==0) return 1;
+        
+        if(t[n][Absent][consecutivelate]!=-1)
+            return t[n][Absent][consecutivelate];
+        
+        int A = solve(n-1,Absent+1,0);
+        int L= solve(n-1,Absent,(consecutivelate+1));
+        int P=solve(n-1,Absent,0);
+        return t[n][Absent][consecutivelate]=((A+L)%M+P)%M;
+    }
+    int checkRecord(int n) 
+    {
+      memset(t,-1,sizeof(t));
+      return solve(n,0,0);
+        
+        
+    }
 };
