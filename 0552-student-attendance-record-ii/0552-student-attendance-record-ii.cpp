a2c1c35db39a1571ec857892d02e467b1b713153
Shivangi Sachan <shivangisachan900@gmail.com>
2024-05-27 11:41:31 +0530

    Time: 25 ms (91.22%), Space: 9.5 MB (89.68%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int M = 1e9+7;
 
-    int checkRecord(int n) {
+int checkRecord(int n) 
+{
         int t[100001][2][3]; 
 
-        //t[i][A][L] = # award possibilities till ith day , having j absence, k consecutive lates
-
         //Base Case - 0 number of days. So, t[0][A][L], we return 1
-        for (int A = 0; A <= 1; ++A) {
-            for (int L = 0; L <= 2; ++L) {
+        for (int A = 0; A <= 1; ++A) 
+        {
+            for (int L = 0; L <= 2; ++L) 
+            {
                 t[0][A][L] = 1;
             }
-        }
+}
 
-        for(int i = 1; i <= n; i++) {
-            for(int A = 0; A <= 1; A++) {
-                for(int L = 0; L <= 2; L++) {
-
+        for(int i = 1; i <= n; i++) 
+        {
+            for(int A = 0; A <= 1; A++) 
+            {
+                for(int L = 0; L <= 2; L++) 
+                {
                     long long result = t[i-1][A][0];         // P ---> solve(n-1, absent, 0) % M;
                     result += (L < 2 ? t[i-1][A][L+1] : 0);  // L ---> solve(n-1, absent, consecutive_late+1) % M;
                     result += (A == 0 ? t[i-1][A+1][0]: 0);  // A ---> solve(n-1, absent+1, 0) % M;
