716d6dc448c90cfa7d95069323a94120518f2809
Shivangi Sachan <shivangisachan900@gmail.com>
2024-05-29 10:47:13 +0530

    Time: 0 ms (100.00%), Space: 8 MB (19.95%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
+    void addOne(string &s) {
+        int i = s.length()-1;
+
+        while(i >= 0 && s[i] != '0') {
+            s[i] = '0';
+            i--;
+        }
+
+        if(i < 0) {
+            s = '1' + s;
+        } else {
+            s[i] = '1';
+        }
+    }
+
     int numSteps(string s) {
+        int op = 0;
 
-	int step = 0;
+        while(s.length() > 1) {
+            int n = s.length();
 
-	while (s != "1") {
+            if(s[n-1] == '0') {
+                s.pop_back();
+            } else {
+                addOne(s);
+            }
 
-		if (s.back() == '0') s.pop_back();
-		else {
+            op++;
+        }
 
-			while (!s.empty() && s.back() == '1') {
-				s.pop_back();
-				++step;
-			}
-
-			if (s.empty()) return step + 1;
-			else s.back() = '1';
-
-		}
-		++step;
-	}
-
-	return step;
-}
+        return op;
+    }
 };
