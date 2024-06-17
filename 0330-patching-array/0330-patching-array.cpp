1d7650dbb8248f23d8d2cdb627f72e0873477ed4
Shivangi Sachan <shivangisachan900@gmail.com>
2024-06-17 11:24:04 +0530

    Time: 4 ms (66.78%), Space: 13.6 MB (82.73%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -1,15 +1,21 @@
-class Solution {
+class Solution 
+{
 public:
-    int minPatches(vector<int>& nums, int n) {
+    int minPatches(vector<int>& nums, int n) 
+    {
         long maxReach = 0;
         int patch     = 0;
         int i         = 0;
 
         while(maxReach < n) {
-            if(i < nums.size() && nums[i] <= maxReach+1) {
+            if(i < nums.size() && nums[i] <= maxReach+1) 
+            {
                maxReach += nums[i];
                i++;
-            } else {
+            } 
+            else 
+        
+            {
                maxReach += (maxReach + 1);
                patch++;
             }
