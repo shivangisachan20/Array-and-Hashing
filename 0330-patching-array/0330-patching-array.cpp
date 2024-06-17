972c6508e8399479322e9890e9759316d0d763ac
Shivangi Sachan <shivangisachan900@gmail.com>
2024-06-17 11:11:07 +0530

    Time: 4 ms (66.78%), Space: 13.8 MB (45.23%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int minPatches(std::vector<int>& nums, int n) {
-    long long patch = 0;
-    int count = 0;
-    int index = 0;
-    while (patch < n) {
-        if (index < nums.size() && patch + 1 >= nums[index]) {
-            patch += nums[index];
-            index++;
-        } else {
-            patch += (patch + 1);
-            count++;
-        }
-    }
+    int minPatches(vector<int>& nums, int n) {
+        long maxReach = 0;
+        int patch     = 0;
+        int i         = 0;
 
-    return count;
-}
+        while(maxReach < n) {
+            if(i < nums.size() && nums[i] <= maxReach+1) {
+               maxReach += nums[i];
+               i++;
+            } else {
+               maxReach += (maxReach + 1);
+               patch++;
+            }
+        }
+        return patch;
+    }
 };
+
+
