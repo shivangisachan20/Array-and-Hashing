6050807bed87e0fc65b500134ec5bea684a1ce6f
Shivangi Sachan <shivangisachan900@gmail.com>
2024-07-07 22:32:36 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (71.99%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int passThePillow(int n, int time) 
-    {
-        int stand = 1, flag = 1;
-        while(time--)
-        {
-            if(flag)
-            {
-                stand++;
-                if(stand == n) flag = !flag;
-            }
-            else 
-            {
-                stand--;
-                if(stand == 1) flag = !flag;
+    int passThePillow(int n, int time) {
+        int idx  = 1; //position of pillow
+        int dir  = 1; //left to right
+
+        while(time > 0) {
+            if(idx + dir >=1 && idx + dir <= n) {
+                idx += dir;
+                time--;
+            } else {
+                dir *= -1;
             }
         }
-        return stand;
+        return idx;
     }
 };
