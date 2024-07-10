14fc9f63b41fb08ae9a63c252ff3a71a16f8703d
Shivangi Sachan <shivangisachan900@gmail.com>
2024-07-10 10:49:58 +0530

    Time: 21 ms (74.62%), Space: 34.4 MB (45.35%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,20 +1,39 @@
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = grumpy.size() ;
-        vector<int> psum1(n + 1) , psum2(n + 2) ;
-        for(int i = 0 ; i <= n - 1 ; i++){
-            psum1[i + 1] = psum1[i] ;
-            if(grumpy[i] == 0){
-                psum1[i + 1] += customers[i] ;
-            }
-            psum2[i + 1] = psum2[i] + customers[i] ;
+        int n = customers.size();
+        int unsat = 0;
+
+        // Calculate initial unsatisfied customers in the first 'minutes' window
+        for (int i = 0; i < minutes; i++) {
+            unsat += customers[i] * grumpy[i];
         }
-        int ans = psum1[n] ;
-        for(int i = 0 ; i + minutes - 1 <= n - 1 ; i++){
-            int sum = psum1[i] + (psum2[i + minutes] - psum2[i]) + psum1[n] - psum1[i + minutes] ;
-            ans = max(ans , sum) ;
+
+        int maxUnsat = unsat;
+
+        // Use two pointers i and j to define the sliding window of size 'minutes'
+        int i = 0;
+        int j = minutes;
+        while(j < n) 
+        {
+            unsat += customers[j] * grumpy[j];             // Include current element
+            unsat -= customers[i] * grumpy[i];             // Remove element going out of window
+            
+            maxUnsat = max(maxUnsat, unsat);               // Update maxUnsat
+            i++;
+            j++;
         }
-        return ans ;
+
+        int totalCustomers = maxUnsat;
+
+        // Calculate total satisfied customers
+        for (int i = 0; i < n; i++) 
+        {
+            if(grumpy[i]==0)
+            totalCustomers += customers[i];
+        }
+        return totalCustomers;
     }
 };
