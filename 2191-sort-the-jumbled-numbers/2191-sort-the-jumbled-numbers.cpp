d525b0b05d24722f230c0b188a6f6d4dd97ec439
Shivangi Sachan <shivangisachan900@gmail.com>
2024-07-24 18:21:32 +0530

    Time: 540 ms (22.51%), Space: 88.9 MB (49.78%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -3,11 +3,13 @@ public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
         vector<pair<int, int>> storePairs;
 
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size(); ++i) 
+        {
             // Convert current value to string
             string number = to_string(nums[i]);
             string formed = "";
-            for (int j = 0; j < number.size(); ++j) {
+            for (int j = 0; j < number.size(); ++j) 
+            {
                 formed = formed + (to_string(mapping[number[j] - '0']));
             }
             // Store the mapped value.
@@ -20,7 +22,8 @@ public:
         // Sort the array in non-decreasing order by the first value (default).
         sort(storePairs.begin(), storePairs.end());
         vector<int> answer;
-        for (auto pair : storePairs) {
+        for (auto pair : storePairs) 
+        {
             answer.push_back(nums[pair.second]);
         }
         return answer;
